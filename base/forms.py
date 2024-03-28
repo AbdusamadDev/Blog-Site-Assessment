0dec02548b751b6796c437c748d658f625c3b9e0
AbdusamadDev <abdusamaddev571@gmail.com>
2024-03-29 01:01:05 +0500

    Pagination

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from ckeditor.widgets import CKEditorWidget
 
 from .models import Blog
 
--- base/views.py
+++ base/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import DetailView, CreateView, TemplateView
 from django.urls import reverse_lazy
- 
+from django.views.generic import (
+    TemplateView,
+    DetailView,
+    CreateView,
+    ListView,
+)
+
 
 from .forms import BlogsForm
 from .models import Blog
@@ -17,6 +22,25 @@ class BlogsCreateView(CreateView):
     form_class = BlogsForm
     success_url = reverse_lazy("home")
 
+
 class HomeView(TemplateView):
     template_name = "home.html"
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        blog_list_view = BlogsListView()
+        blog_list_view.setup(self.request)
+        queryset = blog_list_view.get_queryset()
+        context["object_list"] = queryset
+        paginator = blog_list_view.get_paginator(queryset, blog_list_view.paginate_by)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context["page_obj"] = page_obj
+        context["is_paginated"] = page_obj.has_other_pages()
+        return context
+
+
+class BlogsListView(ListView):
+    template_name = "blog-list.html"
+    paginate_by = 8
+    model = Blog
